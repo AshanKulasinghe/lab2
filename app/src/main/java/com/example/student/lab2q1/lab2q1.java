ac8b97ee40f1cac9c2ad8a6cddc9bff4710f4509
AshanKulasinghe <ashankulasinghe1110@gmail.com>
2019-07-19 13:40:18 +0530

    Commit message goes here

---
+++ app/src/main/java/com/example/student/lab2q1/lab2q1.java
@@ -0,0 +1,13 @@
+package com.example.student.lab2q1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lab2q1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2q1);
+    }
+}
